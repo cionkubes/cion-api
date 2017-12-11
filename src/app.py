4a7c73f2a3e707a75bdd4ef48dadf42acd108f34
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-12-11 17:00:02 +0100

    Added pages to view all and singular services

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import binascii
 import hashlib
 import json
 import random
+import re
 
 import rethinkdb as r
 from aiohttp import web
@@ -40,6 +41,12 @@ def create_hash(to_hash: str):
 
 def hash_str(to_hash: str, salt, iterations):
     return hashlib.pbkdf2_hmac('sha512', to_hash.encode(), salt, iterations, 128)
+
+
+async def image_and_tag(full_image):
+    glob = await get_doc_from_db('glob')
+    r = re.match(glob, full_image)
+    return r.group(0), r.group(1)
 
 
 resp_bad_creds = web.Response(status=401,
@@ -89,9 +96,20 @@ async def api_create_user(request):
     return web.Response(status=201)
 
 
+async def get_doc_from_db(doc_name):
+    return await conn.run(conn.db().table('documents').get(doc_name))
+
+
+async def get_document(request):
+    doc = await get_doc_from_db(request.match_info['name'])
+    return web.Response(status=200,
+                        text=json.dumps(doc),
+                        content_type='application/json')
+
+
 async def set_document(request):
     bod = await request.json()
-    db_res = await conn.run(conn.db().table('documents').get(bod['name']).update(bod))
+    db_res = await conn.run(conn.db().table('documents').get(bod['name']).replace(bod))
     print(db_res)
     return web.Response(status=201)
 
@@ -113,6 +131,48 @@ async def get_documents(request):
     return web.Response(status=200,
                         text=json.dumps(t),
                         content_type='application/json')
+
+
+async def get_service_conf(service_name):
+    db_res = await conn.run(conn.db().table('documents').get('images')['document'])
+    if service_name in db_res:
+        return db_res[service_name]
+    else:
+        return {}
+
+
+async def get_db_running_image(service_name, service_conf=None):
+    if not service_conf:
+        service_conf = await get_service_conf(service_name)
+    if not service_conf:
+        return ''
+    glob = await get_doc_from_db('glob')
+    glob = glob['document']
+
+    def task_filter(task):
+        return task['image-name'].match(glob)['groups'][0]['str'].match(service_conf['image-name'])
+
+    return await conn.run(conn.db().table('tasks')
+                          .filter(task_filter)
+                          .order_by('id')  # should be 'time'
+                          .limit(1)
+                          .pluck('image-name'))
+
+
+async def get_running_image(request):
+    service_name = request.match_info['name']
+    db_res = await get_db_running_image(service_name)
+    if db_res:
+        img_name = db_res[0]['image-name']
+    else:
+        img_name = ''
+    return web.Response(status=200,
+                        text=img_name,
+                        content_type='text/plain')
+
+
+async def get_service(request):
+    pass
 
 
 if __name__ == '__main__':
@@ -145,8 +205,14 @@ if __name__ == '__main__':
 
     app.router.add_post('/api/v1/auth', api_auth)
     app.router.add_post('/api/v1/usercreate', api_create_user)
+
     app.router.add_get('/api/v1/tasks', get_tasks)
+
     app.router.add_get('/api/v1/documents', get_documents)
     app.router.add_post('/api/v1/documents', set_document)
+    app.router.add_get('/api/v1/document/{name}', get_document)
+
+    app.router.add_get('/api/v1/service/image/{name}', get_running_image)
+    app.router.add_get('/api/v1/service/{name}', get_service)
 
     web.run_app(app, host='0.0.0.0', port=5000)
