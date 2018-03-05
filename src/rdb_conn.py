89b6143a8b6540176e66730599dba34f7a7d2f09
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2018-03-05 14:20:15 +0100

    Create indices on database creation

--- src/rdb_conn.py
+++ src/rdb_conn.py
@@ -17,23 +17,39 @@ def init():
     db_host = os.environ['DATABASE_HOST']
     db_port = os.environ['DATABASE_PORT']
 
-    conn = asyncio.get_event_loop().run_until_complete(connection(db_host, db_port))
+    conn = asyncio.get_event_loop().run_until_complete(
+        connection(db_host, db_port))
     asyncio.get_event_loop().run_until_complete(_init_database())
 
 
-async def ensure_table_exists(table_name, primary_key='id', func=None):
+async def ensure_table_exists(table_name, primary_key='id', func=None,
+                              indices=None):
     ret = r.db('cion').table_create(table_name, primary_key=primary_key)
     if func:
         ret = [ret, func]
-    return await conn.run(
+    await conn.run(
         r.db('cion').table_list().contains(table_name).do(
-            lambda table_exists: r.branch(
+            lambda table_exists:
+            r.branch(
                 table_exists,
                 {'tables_created': 0},
                 ret
             )
         )
     )
+    if indices:
+        for index in indices:
+            ret = r.db('cion').table(table_name).index_create(index)
+            await conn.run(
+                r.db('cion').table(table_name).index_list().contains(index).do(
+                    lambda index_exists:
+                    r.branch(
+                        index_exists,
+                        {'index_created': 0},
+                        ret
+                    )
+                )
+            )
 
 
 async def ensure_db_exists(db_name):
@@ -64,7 +80,7 @@ async def _init_database():
     logger.info('Initializing database')
 
     await ensure_db_exists('cion')
-    await ensure_table_exists('tasks')
+    await ensure_table_exists('tasks', indices=['time'])
     with open('default_docs.json', 'r') as default:
         await ensure_table_exists('documents', primary_key='name',
                                   func=r.db('cion').table('documents').insert(
